87270689ad19ef4bd61281eab1437dcc9958a276
beautythieves <96109098+beautythieves@users.noreply.github.com>
2022-11-16 14:40:27 +0100

    fetch final

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,42 +1,27 @@
-
-getPhotographers();
+// getPhotographers();
 
 async function getPhotographers() {
-    fetch("/data/photographers.json")
-    .then(response => response.json())  // convert to json
-    .then(json => console.log(json))   //print data to console
-    //.then((data) => listPhotographers(data))
-    .catch(err => console.log('Request Failed', err)); // Catch errors
-    // .then((response) => response.json())
-    // .then((response) => alert(JSON.stringify(response)))
-    // .catch((error) => alert("Erreur : " + error));
-    // console.log (response.id);
-  // // Penser à remplacer par les données récupérées dans le json
-  const photographers = [
-      {
-          "name": "Ma data testt",
-          "id": 1,
-          "city": "Paris",
-          "country": "France",
-          "tagline": "Ceci est ma data test",
-          "price": 400,
-          "portrait": "account.png"
-      },
-      {
-          "name": "Autre data test",
-          "id": 2,
-          "city": "Londres",
-          "country": "UK",
-          "tagline": "Ceci est ma data test 2",
-          "price": 500,
-          "portrait": "account.png"
-      },
-  ]
-//   // // et bien retourner le tableau photographers seulement une fois
-  return {
-    photographers: [...photographers, ...photographers, ...photographers],
-  };
+  try {
+    const response = await fetch("/data/photographers.json");
+    const json = await response.json();
+    console.table(json);
+    return  {photographers : json.photographers};
+  } catch (error) {
+    console.log("Request Failed", error);
+  }
 }
+
+// function getPhotographersThen() {
+  // fetch("/data/photographers.json")
+  //   .then((response) => response.json()) // convert to json
+  //   .then((json) => console.table(json)) //json type object print data to console
+  //   // .then((data) => photographers(data))
+  //   .catch((err) => console.log("Request Failed", err)); // Catch errors
+  // // .then((response) => response.json())
+  // .then((response) => alert(JSON.stringify(response)))
+  // .catch((error) => alert("Erreur : " + error));
+  // console.log (response.id);
+// }
 
 async function displayData(photographers) {
   const photographersSection = document.querySelector(".photographer_section");
