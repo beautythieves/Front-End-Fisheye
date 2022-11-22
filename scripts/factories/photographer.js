9eeb38115a0353aa1bec8d96e8030263df810149
beautythieves <96109098+beautythieves@users.noreply.github.com>
2022-11-22 18:03:15 +0100

    ajout lieu et prix

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,6 +1,8 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
-
+    //ajout de city, country, price  dans const mais...
+    const { name, portrait, city, country, price } = data;
+    console.log(data)
+    console.log(city);
     const picture = `assets/photographers/${portrait}`;
     console.log(picture  )
     function getUserCardDOM() {
@@ -16,6 +18,13 @@ function photographerFactory(data) {
         $h2.textContent = name;
         $article.appendChild($img);
         $article.appendChild($h2);
+        //porquoi textcontent ne marchait pas mais innerhtml oui?
+        const $h3 = document.createElement( 'h3' );
+        $h3.innerHTML= city + " , " + country;
+        $h2.appendChild($h3)
+        const $h4 = document.createElement( 'h4' );
+        $h4.textContent = price + "€/jour";
+        $h3.appendChild($h4)
         return ($article);
     }
     return { name, picture, getUserCardDOM }
