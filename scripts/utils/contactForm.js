1de2a2607d6f4d2411a3628e03e7e07fcb454bd2
beautythieves <96109098+beautythieves@users.noreply.github.com>
2022-12-12 11:45:17 +0100

    modified

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -37,10 +37,11 @@ export function displayModal() {
     };
 
 	
-
+/*
 
 function closeModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
 }
 
+*/
